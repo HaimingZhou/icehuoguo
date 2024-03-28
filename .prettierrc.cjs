6798c25f6be78caa21384dfc89a31c55087930e0
Zhou Haiming <13236613225@163.com>
2024-03-28 17:45:08 +0800

    build: 补充tailwind配置

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,7 @@
 module.exports = {
+  printWidth: 100,
   singleQuote: true,
   tabWidth: 2,
-  semi: false
-};
+  semi: false,
+  plugins: ['prettier-plugin-organize-imports'],
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: 'jit', // 允许在运行时生成新的实用程序类,目的是为了动态生成类名
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
